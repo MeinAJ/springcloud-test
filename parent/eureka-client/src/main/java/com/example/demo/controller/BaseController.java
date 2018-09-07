06030a845a5cecdd447fa741ca2ee265e25ddd88
MeinAJ <13594032148@163.com>
2018-09-07 11:01:39 +0800

    新增一个登陆功能

---
+++ parent/eureka-client/src/main/java/com/example/demo/controller/BaseController.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class BaseController {
+
+    protected JSONObject json = new JSONObject();
+}
--- parent/eureka-server/src/main/java/com/aj/service/server/EurekaServerApplication.java
+++ parent/eureka-server/src/main/java/com/aj/service/server/EurekaServerApplication.java
@@ -1,8 +1,7 @@
-package com.example.demo;
+package com.aj.service.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
