138c14966ec04834f0e2137981e15fd0fd33419e
MeinAJ <13594032148@163.com>
2018-09-15 23:04:41 +0800

    更新

--- parent/eureka-client/src/main/java/com/example/demo/EurekaClientApplication.java
+++ parent/eureka-client/src/main/java/com/example/demo/EurekaClientApplication.java
@@ -2,11 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import javax.swing.*;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class EurekaClientApplication {
+public class EurekaClientApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
--- parent/eureka-rabbitmq/src/main/java/com/aj/rabbitmq/EurekaRabbitMqApplication.java
+++ parent/eureka-rabbitmq/src/main/java/com/aj/rabbitmq/EurekaRabbitMqApplication.java
@@ -2,11 +2,12 @@ package com.aj.rabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class EurekaRabbitMqApplication {
+public class EurekaRabbitMqApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaRabbitMqApplication.class, args);
